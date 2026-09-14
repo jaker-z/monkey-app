21711a424bf0a11312195044d7a6dcffbee8e530
Jake Hope <jacob.hope@icloud.com>
2026-09-14 10:53:12 -0700

    Tweaked BaseViewModel to become partial class with ObservableObject

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -2,12 +2,73 @@
 
 public partial class BaseViewModel : ObservableObject
 {
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
-    public partial bool IsBusy { get; set; }
+    public BaseViewModel()
+    {
+
+    }
 
     [ObservableProperty]
-    public partial string Title { get; set; }
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
 
     public bool IsNotBusy => !IsBusy;
 }
+
+
+
+//public partial class BaseViewModel : ObservableObject
+//{
+//    [ObservableProperty]
+//    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+//    public partial bool IsBusy { get; set; }
+
+//    [ObservableProperty]
+//    public partial string Title { get; set; }
+
+//    public bool IsNotBusy => !IsBusy;
+//}
+
+
+/* BASICS OF VIEW MODEL, REPLACED BY SHORTER CODE
+public partial class BaseViewModel : INotifyPropertyChanged
+{
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+    
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsNotBusy => !IsBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName]string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+*/
